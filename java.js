9697db703c311f74e819c94f43c2a08e5c94fb0c
xellox <korpasdarius911@gmail.com>
2023-03-04 17:06:37 +0200

    modified:   index.html 	modified:   java.js 	modified:   style.css

--- java.js
+++ java.js
@@ -1,23 +1,44 @@
 // API URL https://api.openweathermap.org/data/2.5/weather?q=Oradea&units=metric&appid=73ca880f94b5f59ea0326ae3995abf02 //
 // API KEY 73ca880f94b5f59ea0326ae3995abf02 //
+let weather = {
+    apiKey: '73ca880f94b5f59ea0326ae3995abf02',
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            +"&units=metric&appid=" 
+            + this.apiKey   
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    }, 
 
-
-let city = document.querySelector('.cityname')
-let description = document.querySelector('.description')
-let temperature = document.querySelector('.degrees')
-let wind = document.querySelector('.wind')
-let humidity = document.querySelector('.humidity')
-let apiKey =  '73ca880f94b5f59ea0326ae3995abf02'
-let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.textContent + "&units=metric&appid=" + apiKey;
-let button = document.getElementById('button')
-
-async function myFunction(weatherUrl){
-    const response = await fetch(weatherUrl);
-    const data = await response.json();
-        const temp = data.main;
-        const showTemp = temp.temp;
-        console.log(showTemp)
-        temperature.append(showTemp)
-        console.log(temperature)
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, humidity,temp,speed)
+        document.querySelector('.cityname').innerText = 'Weather in ' + name;
+        document.querySelector('.icon').src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector('.description').innerText = description;
+        document.querySelector('.degrees').innerText = Math.round(temp) + ' C°'
+        document.querySelector('.wind').innerText = 'wind speed: ' + Math.round(speed) + ' km/h';
+        document.querySelector('.humidity').innerText = 'humidity: ' + humidity + '%';
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector("#searchinput").value);
+        document.querySelector("#searchinput").value = ''
+      },
+                                                               
 }
 
+document.querySelector("#button").addEventListener("click", function () {
+    weather.search();
+  });
+
+document.querySelector("#searchinput").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      weather.search();
+    }
+  });
