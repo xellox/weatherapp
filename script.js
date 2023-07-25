7132f1da237df59dd4ff84578971a0daeafb46ac
xellox <korpasdarius911@gmail.com>
2023-07-25 10:55:55 +0300

    modified:   bg.jpg 	deleted:    bg2.jpg 	deleted:    bg3.jpg 	modified:   index.html 	deleted:    samuel-ferrara-1527pjeb6jg-unsplash.jpg 	modified:   script.js 	modified:   style.css 	deleted:    weatherBg.jpg

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let searchbar = document.querySelector(".searchbar-input")
-
+let gridWeather = document.querySelector(".grid-weather")
 async function fetchData(type){
-    const res = await fetch("https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=Vbv06QeVWbLeP50Ow3KMC8ca2NdrWtSX")
-    const data = await res.json();
-    console.log(data)
 
-    let url = ""
+    let APIkey = '73ca880f94b5f59ea0326ae3995abf02';
+    let city = searchbar.value;
+    let url = "";
     switch(type) {
-        case "daily":
-            url= "https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature,windSpeed,precipitationProbability&timesteps=1d&units=metric&apikey=Vbv06QeVWbLeP50Ow3KMC8ca2NdrWtSX"
+        case "current":
+            url= "https://api.openweathermap.org/data/2.5/weather?id=" + city + "&units=metric&appid=" + APIkey;
             break;
         
         case "hourly":
-            url="https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature,windSpeed,precipitationProbability&timesteps=1h&units=metric&apikey=Vbv06QeVWbLeP50Ow3KMC8ca2NdrWtSX"
+            url="http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=" + APIkey;
             break
                 default:
             return;
+
     }
-    try{    
+
+    try{
         const res= await fetch(url)
         const data= await res.json()
         displayWeather(data)
@@ -27,13 +28,56 @@ async function fetchData(type){
 }
 
 
-function displayWeather(data){
-    let test = data.data.timelines[0].intervals[0].values.temperature;
-    console.log(test)
-}
+function displayWeather(data) {
+    let city = searchbar.value;
+    gridWeather.innerHTML = "";
+  
+    const weatherByDate = {};
+  
+    // Group weather data by date
+    data.list.forEach((interval) => {
+      const date = new Date(interval.dt_txt);
+      const day = date.toLocaleDateString('en-US', { day: 'numeric' });
+      const month = date.toLocaleDateString('en-US', { month: 'short' });
+      const year = date.getFullYear();
+      const dateString = `${day} ${month} ${year}`;
+  
+      if (!weatherByDate[dateString]) {
+        weatherByDate[dateString] = {
+          temperatures: [interval.main.temp],
+          description: interval.weather[0].description,
+          icon: interval.weather[0].icon,
+        };
+      } else {
+        weatherByDate[dateString].temperatures.push(interval.main.temp);
+      }
+    });
+  
+    // Generate HTML content
+    for (const [dateString, weatherData] of Object.entries(weatherByDate)) {
+      const date = new Date(dateString);
+      const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+      const maxTemp = Math.max(...weatherData.temperatures);
+      const minTemp = Math.min(...weatherData.temperatures);
+
+      gridWeather.innerHTML += `
+        <div class="next-days">
+          <span class="next-day">${day}</span>
+          <img src="https://openweathermap.org/img/wn/${weatherData.icon}.png" class="next-icon-temp"></img>
+          <div class="next-description">
+            <span class="next-temp">${Math.round(maxTemp)}°C</span>
+            <small>${Math.round(minTemp)}°C</small>
+            <span class="next-desc">${weatherData.description}</span>
+          </div>
+        </div>
+      `;
+    }
+  }
+
+
 
 searchbar.addEventListener("keypress", (e) => {
     if ((e.key === "Enter") & (searchbar.value != "")) {
-      fetchData("daily");
+      fetchData("hourly");
     }
   });
